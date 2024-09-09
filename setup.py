00028dbb1c4e9bb335e533a306f9ba9cd3431db4
Andrey Shalashov <avshalashov@yandex.ru>
2024-09-09 21:15:38 +0300

    up version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox_scripthelper',
-    version='0.2.0',
+    version='0.3.0',
     description='Collections of utilities for Netbox custom scripts.',
     url='https://github.com/k1nky/netbox-scripthelper-plugin',
     author='Andrey Shalashov',
     author_email='avshalashov@yandex.ru',
     license='Apache 2.0',
-    keywords='netbox script custom helper plugin',
+    keywords='netbox-scripts custom helper plugin',
     install_requires=[
         'netaddr'
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=["*tests.*", "*tests"]),
     package_data={},
     include_package_data=True,
     zip_safe=False,
